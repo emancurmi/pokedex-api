6939958264478cef0f3b9db0b29f4eb6f9621f1c
Eman Curmi <emancurmi@gmail.com>
2020-06-16 07:28:18 -0400

    Middleware pipelines for each station

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express()
 
 app.use(morgan('dev'))
 
+
+
+
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
 //7b967bbe-afc2-11ea-b3de-0242ac130004
@@ -16,13 +19,13 @@ function handleGetTypes(req, res) {
     res.json(validTypes)
 }
 
-app.get('/types', handleGetTypes)
+app.get('/types', validateBearerToken, handleGetTypes)
 
 function handleGetPokemon(req, res) {
     res.send('Hello, Pokemon!')
 }
 
-app.get('/pokemon', handleGetPokemon)
+app.get('/pokemon', validateBearerToken, handleGetPokemon)
 
 const PORT = 8000
 
