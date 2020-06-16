62be88ee742734b3e7271c14879e9639006af578
Eman Curmi <emancurmi@gmail.com>
2020-06-16 07:22:22 -0400

    Secret API tokens and environmental variables

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+//7b967bbe-afc2-11ea-b3de-0242ac130004
 
 function handleGetTypes(req, res) {
     res.json(validTypes)
